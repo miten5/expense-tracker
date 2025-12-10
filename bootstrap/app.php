b08e3046b43a9c4717ee14daf57c512cd94f3da5
Miten Patel <patelmit3n@gmail.com>
2025-12-09 21:56:32 -0500

    feat: add customer exception handle response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,39 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . "/../routes/web.php",
+        api: __DIR__ . "/../routes/api.php",
+        commands: __DIR__ . "/../routes/console.php",
+        health: "/up",
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (
+            \Illuminate\Database\Eloquent\ModelNotFoundException $e,
+            $request,
+        ) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Resource not found",
+                ],
+                404,
+            );
+        });
+
+        $exceptions->render(function (
+            \Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e,
+            $request,
+        ) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Route not found",
+                ],
+                404,
+            );
+        });
+    })
+    ->create();
